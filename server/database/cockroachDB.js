0e6dc5c5383f092d4d762827aeb4376880ca853c
Artem Tkachuk <artemtkachuk@yahoo.com>
2019-10-26 11:32:33 -0700

    Almost done with cockroachDB
    The only thing left is to create an event model and do CRUD on it

--- server/database/cockroachDB.js
+++ server/database/cockroachDB.js
@@ -1,42 +1,10 @@
 const Sequelize = require('sequelize-cockroachdb');
-
+const fs = require('fs');
 const Event = require('../models/event'); //sequelize model of event
-//const firstUser = require('../keys/firstUser');
-const cockroackUri = require('../config/cockroachDB');
+const cockroackDBConfig = require('../config/cockroachDBconfig');
 
-// const config = require('../config/cockroachDB');
-// const {name, username, password} = require('../keys/credentials');
-
-const sequelize = new Sequelize(cockroackUri);
-
-const sequelizeSync = (app, port) => {
-    sequelize   //Testing the DB connection
-        .authenticate()
-        .then(() => {
-            console.log('Connection has been established successfully!');
-        })
-        .catch(err => {
-            console.log('Unable to connect to the database', err);
-        });
-
-    // sequelize.sync(/*{force: true}*/)
-    //     .then(result => {
-    //         return User.findByPk(1);
-    //     })
-    //     .then(async (user) => {
-    //         if (!user) {
-    //             firstUser.password = await hash(firstUser.password);
-    //             return User.create(firstUser);
-    //         }
-    //         return user;
-    //     })
-    //     .then(user => {
-    //         console.log('users table now exists!');
-    //     })
-    //     .catch(err => console.log(err));
-};
+const cockroachDB = new Sequelize(cockroackDBConfig.db, cockroackDBConfig.user, cockroackDBConfig.password, cockroackDBConfig.dialectOptions);
 
 module.exports = {
-    sequelize: sequelize,
-    sequelizeSync: sequelizeSync
+    cockroachDB: cockroachDB
 };
